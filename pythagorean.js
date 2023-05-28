1e2f8c65b19452e496116f04162911fccd09ed15
Tuan Anh Nguyen <atngu312@gmail.com>
2023-05-27 23:45:24 -0400

    add pythagorean stretch week 2

---
+++ pythagorean.js
@@ -0,0 +1,10 @@
+const input = [
+  { x: 3, y: 4 },
+  { x: 12, y: 5 },
+  { x: 8, y: 15 }
+];
+const result = input.map(pair => Math.pow((Math.pow(pair.x, 2) + Math.pow(pair.y, 2)), 0.5));
+
+console.log(result[0] === 5);
+console.log(result[1] === 13);
+console.log(result[2] === 17);
